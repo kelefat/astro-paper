4298811d33c4ee9ad96f3d06f940f99e5aed3549
kelefat <kelefat@hotmail.com>
2025-04-28 16:34:38 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ export const SITE: Site = {
   title: "Kelefat",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 3,
+  postPerIndex: 10,
+  postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
 };
